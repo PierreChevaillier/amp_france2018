9ec41bcb36305127e9e52ba402d00689661c4a9f
Pierre Chevaillier <pierre.chevaillier@enib.fr>
2017-06-21 08:26:51 +0200

    actualites - logo AMP carre comme icone

--- actualites.php
+++ actualites.php
@@ -26,12 +26,20 @@
       $page->contenus[] = new Contenu_Actualites($journal, new Vue_Contacts());
       
       // --- les actualites
+      $actu = new Actualite("Passage de relais à Marseille");
+      $actu->categorie = $comm;
+      $actu->date = "17 juin 2017";
+      $actu->nom_fichier_vignette = "logo-amp-carre-64x64.png";
+      $actu->contenu = "Ce week-end à l’occasion du Championnat de France d’Aviron de Mer 2017 qui se déroulait à Marseille, l’Aviron de Mer de Plougonvelin a été officiellement annoncé comme l’organisateur de l’édition 2018.<br />A Cette occasion Joël Champeau, président de l’AMP et Xavier Hervé, président du comité local d’organisation ont dévoilé l’affiche de l’édition 2018. Le Club est déjà mobilisé pour cet événement majeur et met toute son énergie pour l’organisation de cet événement.";
+      $journal->actualites[] = $actu;
+     
       $actu = new Actualite("Lancement du site web");
       $actu->categorie = $comm;
       $actu->date = "13 juin 2017";
       $actu->nom_fichier_vignette = "vignette_site_fr2018.png";
       $actu->contenu = "L'AMP est fier de lancer le site web de l'Edition 2018 du Championnat de France d'aviron de mer. Ce n'est qu'un début... Rendez-nous visite régulièrement pour d'autres nouvelles.";
       $journal->actualites[] = $actu;
+      
       
       $page->initialiser();
       $page->afficher();
